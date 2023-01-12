aa2b54a5a57547b4fad25035c9f3ade32c8528b2
Mercator <Mercator.poei@gmail.com>
2023-01-12 14:19:39 +0100

    JAVA exercice 3

---
+++ Mercator/tp2.java
@@ -0,0 +1,38 @@
+package Mercator;
+
+import java.util.Scanner;
+
+public class tp2 {
+
+    public static void main(String[] args) {
+        Scanner keyboardInput = new Scanner(System.in);
+        System.out.print("Tapez le chiffre correspondant a un jour :");
+        int num1 = keyboardInput.nextInt();
+        switch(num1) {
+            case 1:
+                System.out.println("Lundi");
+                break;
+            case 2:
+                System.out.println("Mardi");
+                break;
+            case 3:
+                System.out.println("Mercredi");
+                break;
+            case 4:
+                System.out.println("Jeudi");
+                break;
+            case 5:
+                System.out.println("Vendredi");
+                break;
+            case 6:
+                System.out.println("Samedi");
+                break;
+            case 7:
+                System.out.println("Dimanche");
+                break;
+            default:
+                System.out.println("=== NOK ===");
+        }
+
+    }
+}
